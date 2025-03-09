ee0542213b596210c38f580e68fb435327eed84d
hqluong <65425545+hqluong@users.noreply.github.com>
2025-03-09 15:07:46 +0700

    Initial Commit

---
+++ DailyMotivation/DailyMotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotivationApp.swift
+//  DailyMotivation
+//
+//  Created by Hung Luong on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyMotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyMotivationTests/DailyMotivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotivationTests.swift
+//  DailyMotivationTests
+//
+//  Created by Hung Luong on 3/9/25.
+//
+
+import Testing
+@testable import DailyMotivation
+
+struct DailyMotivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
